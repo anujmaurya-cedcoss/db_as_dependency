c1d469150f417e09d61a9512030534200e915cb1
Anuj Maurya <anujmaurya@cedcoss.com>
2023-06-29 15:41:12 +0530

    initial commit

---
+++ application/app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+return new \Phalcon\Config([
+    'database' => [
+        'adapter'     => 'Mysql',
+        'host'        => 'localhost',
+        'username'    => 'root',
+        'password'    => 'secret',
+        'dbname'      => 'test',
+        'charset'     => 'utf8',
+    ],
+]);
